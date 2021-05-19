6a56f403984af88d3b4f09b758022bb6981919ff
grimsley217 <84026008+grimsley217@users.noreply.github.com>
2021-05-19 00:33:02 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -13,16 +13,16 @@ def median(X):
   y=len(X) #calculates the number of items in the list
   z=y-1 #decrease the length by 1; is useful later if the list count is odd
   return (X[y//2]+X[z//2])/2
-#[ ] searches the index, so since indexes start at 0, [number] looks at the 'number+1' value in the list
+#List[ ] searches that list and returns the value at the [ ] index, so since indexes start at 0, [number] looks at the 'number+1'th value in the list
 #if there are an odd number of elements, the indexes y//2 and z//2 are the same value, so dividing the sum by 2 returns the same middle value
 #if there are an even number of elements, the middle indexes are y//2 and z//2 respectively, so dividing the sum by 2 returns the average of the two middle values
 
 #creating function to help natively find the mode
-def mode(example2):
+def mode(N):
 
 
 print('The count is ' + str(a) + '.')
 print('The sum is ' + str(b) + '.')
 print('The mean is ' + str(c) + '.')
-print('The median is ' + median(values) + '.')
-print('The mode is ' + mode(values) + '.')
+print('The median is ' + str(median(values)) + '.')
+print('The mode is ' + str(mode(values)) + '.')
