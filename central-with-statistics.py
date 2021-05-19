e86085b52eee4b39e3635a4c37aa4126b0f38544
grimsley217 <84026008+grimsley217@users.noreply.github.com>
2021-05-19 00:35:42 -0500

    Update central-with-statistics.py

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -1,1 +1,17 @@
+#central-with-statistics.py
+"""The following calculates count, sum, mean, median, and mode WITH the statistics module."""
 
+import statistics
+values = [47, 95, 88, 73, 88, 84]
+
+a=len(values)
+b=sum(values)
+c=statistics.mean(values)
+d=statistics.median(values)
+e=statistics.mode(values)
+
+print('The count is ' + str(a) + '.')
+print('The sum is ' + str(b) + '.')
+print('The mean is ' + str(c) + '.')
+print('The median is ' + str(d) + '.')
+print('The mode is ' + str(e) + '.')
