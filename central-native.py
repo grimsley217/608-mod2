8a975dae130cc81d7c3801fd9b811c027327782d
grimsley217 <84026008+grimsley217@users.noreply.github.com>
2021-05-19 01:11:38 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -13,14 +13,25 @@ def median(X):
   y=len(X) #calculates the number of items in the list
   z=y-1 #decrease the length by 1; is useful later if the list count is odd
   return (X[y//2]+X[z//2])/2
-#List[ ] searches that list and returns the value at the [ ] index, so since indexes start at 0, [number] looks at the 'number+1'th value in the list
-#if there are an odd number of elements, the indexes y//2 and z//2 are the same value, so dividing the sum by 2 returns the same middle value
-#if there are an even number of elements, the middle indexes are y//2 and z//2 respectively, so dividing the sum by 2 returns the average of the two middle values
+#List[ ] searches that list and returns the value at the [ ] index
+#since indexes start at 0,#[number] looks at the 'number+1'th value in the list
+
+#if there are an odd number of elements, the indexes y//2 and z//2 are the same
+#so dividing the sum by 2 returns the same middle value
+
+#if there are an even number of elements, the middle indexes are y//2 and z//2
+#dividing the sum by 2 returns the average of the two middle values
 
 #creating function to help natively find the mode
 def mode(N):
+  return max(N, key=N.count)
+#the max function with a key returns the max value in a list
+#based on the desired criteria expressed in the key
 
+#this max function uses count as the key, so the function look at our list
+#and returns the value with the highest count total within the list
 
+print(values)
 print('The count is ' + str(a) + '.')
 print('The sum is ' + str(b) + '.')
 print('The mean is ' + str(c) + '.')
